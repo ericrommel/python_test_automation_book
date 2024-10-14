66b065bf9ac1999ffdc6c0d537b80277adc7da5b
Sergei Smirnov6 <sergei_smirnov6@epam.com>
2024-10-15 00:18:47 +0200

    added regex 3_1

---
+++ weeks/completed_tasks_sergei/week03/week03_regex_3_1.py
@@ -0,0 +1,30 @@
+import re
+
+# Define if a string contains the required characters.
+# E.g. if '7865serS3' includes '583' - True; '973' - False
+
+regex_one = "583"
+regex_two = "973"
+string_one = "7865serS3"
+
+
+def all_chars_are_in_string(regex_string, test_string):
+    for x in regex_string:
+        # if re.search(re.escape(x), test_string):
+        if re.search(re.compile(x), test_string):
+            continue
+        else:
+            return False
+    return True
+
+    # return all(re.search(re.escape(char), test_string) for char in regex_string)
+
+
+def main():
+    print(all_chars_are_in_string(regex_one, string_one))
+    print(all_chars_are_in_string(regex_two, string_one))
+    print(all_chars_are_in_string("res", string_one))
+
+
+if __name__ == "__main__":
+    main()
