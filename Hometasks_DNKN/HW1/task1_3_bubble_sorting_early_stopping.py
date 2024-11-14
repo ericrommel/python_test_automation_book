ae6668f6a70d1bccafe1be87dd071464d91e4dde
Iryna Dankina <iryna_dankina@epam.com>
2024-11-14 23:54:36 +0100

    added 3 tasks from the homework

---
+++ Hometasks_DNKN/HW1/task1_3_bubble_sorting_early_stopping.py
@@ -0,0 +1,23 @@
+'''
+Research about the Bubble Sort Algorithm in Python. Implement three version of it:
+With early stopping
+'''
+import random
+
+def bubble_sort_early_stopping(random_numbers):
+    n = len(random_numbers)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if random_numbers[j] > random_numbers[j+1]:
+                random_numbers[j], random_numbers[j+1] = random_numbers[j+1], random_numbers[j]
+                swapped = True
+        if not swapped:
+            break
+    return random_numbers
+
+
+random_numbers = [random.randint(0, 100) for k in range(20)]
+print(f'a list of 20 random numbers from 0 to 20: {random_numbers}')
+
+print(f'random numbers sorted with early stopping: {bubble_sort_early_stopping(random_numbers)}')
